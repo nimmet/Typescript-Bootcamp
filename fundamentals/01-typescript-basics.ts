63e2e9161e87534e868ccff334974cfe6a2fc4cb
Nimetullah Necmettin <91189127+nimmet@users.noreply.github.com>
2024-06-21 17:37:07 +0200

    create typescript fundamental project

---
+++ fundamentals/01-typescript-basics.ts
@@ -0,0 +1,8 @@
+const courseName = "TypeScript Bootcamp";
+
+printCourseName(courseName);
+printCourseName("Java Spring");
+
+function printCourseName(name :string){
+    console.log("The course name is: "+name.toUpperCase());
+}
