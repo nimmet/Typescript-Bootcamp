bc1babbbcaecb842dbf7386d82270e89dc19525d
Nimetullah Necmettin <91189127+nimmet@users.noreply.github.com>
2024-06-21 21:01:55 +0200

    typescript fuundamentals: primitive data types,objects,string,numbers

---
+++ fundamentals/02-primitive-types.js
@@ -0,0 +1,16 @@
+var lessonsCount = 10;
+var total = lessonsCount + 10;
+console.log("total = ", total);
+var title = "TypeScript Bootcamp";
+var subTitle = "Learn fundamentals of typescript";
+var fullTitle = title + " " + subTitle;
+var ft = "Full title: ".concat(title, ": ").concat(subTitle);
+console.log("Full title: " + fullTitle);
+console.log(ft);
+// primitive type: object
+var course = {
+    title: "Spring mvc",
+    subtitle: "Learn fundamentals of typescript",
+    lessonsCount: 10
+};
+console.log(course.title);
